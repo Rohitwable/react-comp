bc9fa38fe7f99652c168a33e04283620cb5ffb3d
Rohitwable <rohitwable46@gmail.com>
2020-01-25 15:26:32 +0530

    inline style / added conditionals

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ class App extends React.Component {
       { name: "person2", age: "23" },
       { name: "person3", age: "44" },
     ],
-    untouched: "this will not effect"
+    untouched: "this will not effect",
+    showPersons: false,
   }
   switchNameHandler = (newName) => {
     this.setState({
@@ -32,22 +33,36 @@ class App extends React.Component {
       ]
     })
   }
+  togglePersonsHandler = () => {
+    this.setState({ showPersons: !this.state.showPersons })
+  }
   render() {
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer",
+    }
     return (
       <div className="App">
         <p>App component</p>
-        <button onClick={this.switchNameHandler.bind(this, "Rohit")}>Switch name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-          clicked={() => this.switchNameHandler("person")} />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}>Hobbies: Racing</Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-          changed={this.nameChangeHandler} />
+        <button style={style} onClick={this.togglePersonsHandler}>Switch name</button>
+        {this.state.showPersons ?
+          <div>
+            <Person
+              name={this.state.persons[0].name}
+              age={this.state.persons[0].age}
+              clicked={() => this.switchNameHandler("person")} />
+            <Person
+              name={this.state.persons[1].name}
+              age={this.state.persons[1].age}>Hobbies: Racing</Person>
+            <Person
+              name={this.state.persons[2].name}
+              age={this.state.persons[2].age}
+              changed={this.nameChangeHandler} />
+          </div>
+          : ''}
       </div>
     )
   }
