899950fa9171165e2ad10daa23feb52d63e86fa7
Rohitwable <rohitwable46@gmail.com>
2020-01-25 14:26:32 +0530

    dynamic content

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -10,7 +10,7 @@ import React from 'react';
 // }
 
 const person = () => {
-    return <p>Person Component</p>
+    return <p>I am {Math.floor(Math.random() * 30)} years old.</p>
 }
 export default person;
 
