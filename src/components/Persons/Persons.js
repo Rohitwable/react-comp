300929ce9e2de70614c64c921eb76cd55b26fd75
Rohitwable <rohitwable46@gmail.com>
2020-01-25 22:14:47 +0530

    performance optimization with shoulComponentUpdate

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import Person from './Person/Person';
 
-const persons = (props) => {
-    console.log("person.js render");
-    return props.persons.map((value, index) =>
-        <Person
-            key={index}
-            name={value.name}
-            age={value.age}
-            clicked={() => props.clicked(index)}
-            changed={(event) => props.changed(event, value.id)} />
-    );
+class Persons extends React.Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        if (nextProps.persons !== this.props.persons) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    render() {
+        console.log("person.js render");
+        return this.props.persons.map((value, index) =>
+            <Person
+                key={index}
+                name={value.name}
+                age={value.age}
+                clicked={() => this.props.clicked(index)}
+                changed={(event) => this.props.changed(event, value.id)} />
+        );
+    }
 };
 
-export default persons;
+export default Persons;
