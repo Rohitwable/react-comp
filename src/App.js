ae04b759566df91ec556f114b9eb4605ca932df3
Rohitwable <rohitwable46@gmail.com>
2020-01-25 14:29:42 +0530

    passing props

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
   return (
     <div className="App">
       <p>App component</p>
-      <Person />
+      <Person name="person1" age="30" />
+      <Person name="person2" age="12" />
+      <Person name="person3" age="23" />
     </div>
   )
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -9,8 +9,8 @@ import React from 'react';
 
 // }
 
-const person = () => {
-    return <p>I am {Math.floor(Math.random() * 30)} years old.</p>
+const person = (props) => {
+    return <p>I am {props.name} and {props.age} years old.</p>
 }
 export default person;
 
